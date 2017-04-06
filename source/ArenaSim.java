7d4d6b8ea8455068a91fbb443c42265811023cbe
Lucas Karahadian <lucask4@gmail.com>
2017-04-06 16:23:53 -0700

    Added Output results showing total win odds

--- source/ArenaSim.java
+++ source/ArenaSim.java
@@ -7,14 +7,26 @@ public class ArenaSim {
 
 	public static void main(String[] args) {
 		float[][] fullArena = batchCompute();
-		// System.out.printf("%15s\n", "Losses");
+		System.out.printf("%25s\n", "Losses");
+		System.out.printf("%-8s", "Wins");
+		for (int i = 0; i <= 3; i++) {
+			System.out.printf("%-8d", i);
+		}
+		System.out.println();
 		for (int i = 0; i < fullArena.length; i++) {
+			System.out.printf("%-8d", i);
 			for (int j = 0; j < fullArena[0].length; j++) {
 				String inner = String.format("%.2f%%", fullArena[i][j]);
 				System.out.printf("%-8s", inner);
 			}
 			System.out.println();
 		}
+		System.out.println("============================");
+		System.out.println("Win Percentages Over All Arena Runs");
+		for (int i = 0; i < 12; i++) {
+			System.out.printf("%-4s%.2f%%%n", String.format("%d:", i), fullArena[i][3]);
+		}
+		System.out.printf("%-4s%.2f%%%n", String.format("%d:", 12), fullArena[12][0] + fullArena[12][1] + fullArena[12][2]);
 	}
 
 	private static float[][] batchCompute() {
